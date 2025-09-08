22a50d0c271ec2639f176e8487492ef5b1114f44
depaa81 <xiyennnyennn@gmail.com>
2025-09-08 21:46:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-let audio;
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
 
-function playMusic() {
-    if (!audio) {
-        audio = new Audio('music/love-song.mp3');
-    }
-    audio.play();
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
 }
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Musik
+let audio;
+function playMusic() {
+  if (!audio) {
+    audio = new Audio('music/love-song.mp3');
+  }
+  audio.play();
+}
+
+// tampilkan foto pertama saat load
+window.onload = () => {
+  showSlide(currentSlide);
+};
